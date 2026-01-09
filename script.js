bd0b45df4bab147d4284ecdedd516ae500501961
sonalshinde24 <sonal2004shinde@gmail.com>
2026-01-09 23:36:46 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function filterProjects(category) {
+  let cards = document.querySelectorAll('.project-card');
+
+  cards.forEach(card => {
+    if (category === 'all' || card.classList.contains(category)) {
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+}
